2efd573fe1a29d0fe5117831ae1b9e5e4c12c6be
maksim <kuzminowmax@gmail.com>
2022-05-21 20:04:01 +0300

    fix: 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+const fsPromises = fs.promises;
 
-fs.mkdir(
-    path.join(__dirname, 'files-copy'),
-    (err) => {
-        if (err) throw err;
+const FilesPath = path.join(__dirname, 'files');
+const FilesCopyPath = path.join(__dirname, 'files-copy');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+  if (err) {console.log('Папка уже создана');}
+
+});
+async function copyDir() {
+    const filesCopy = await fsPromises.readdir(FilesCopyPath,  { withFileTypes: true });
+    for (let item of filesCopy) {
+      const copyItemPath = path.join(FilesCopyPath, item.name);
+      fs.unlink(`${copyItemPath}`)
+
+const files = await fsPromises.readdir(FilesPath,  { withFileTypes: true });
+  for (let item of files) {
+    const currentItemPath = path.join(FilesPath, item.name);
+    const copyItemPath = path.join(FilesCopyPath, item.name);
+    await fsPromises.copyFile(currentItemPath, copyItemPath);
 }
-    )
+
+}
+copyDir();
